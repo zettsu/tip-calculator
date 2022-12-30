ea8ff8fea6b4830c40e82a4fc97b039f1ee8e9af
Matias Olivera <jmatias.olivera@gmail.com>
2022-12-30 19:34:59 -0300

    added validations and some fixes

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-let bill = 0;
-let people = 0;
 let tipAmount = 0;
 
-let billInput = document.getElementById('bill');
-let peopleInput = document.getElementById('people');
-let tipAmountInputs = document.querySelectorAll('input[type=radio][name=tip-amount]');
+let tipAmountRadios = document.querySelectorAll('input[type=radio][name=tip-amount]');
 let tipAmountCustom = document.getElementById('tip-amount-custom');
-
-let tipPlaceholder = document.getElementById('tip-amount-person-placeholder')
+let tipPlaceholder  = document.getElementById('tip-amount-person-placeholder')
 let totalPlaceholder = document.getElementById('total-amount-person-placeholder')
 
 
@@ -15,59 +10,97 @@ tipAmountCustom.addEventListener('focus', () => {
     uncheckRadios()
 });
 
+function cleanErrorMessages() {
+    document.querySelectorAll(`.error-msg`).forEach(msg => msg.innerText = "")
+}
+
 tipAmountCustom.addEventListener('input', (evt) => {
     tipAmount = evt.target.value / 100;
     calculate()
 });
 
-peopleInput.addEventListener('input', (evt) => {
-    people = parseInt(evt.target.value);
+document.getElementById('people').addEventListener('input', (evt) => {
     calculate()
 });
 
-billInput.addEventListener('change', (evt) => {
-    bill = parseInt(evt.target.value);
+document.getElementById('bill').addEventListener('change', () => {
     calculate()
 });
 
-tipAmountInputs.forEach(radio => {
+tipAmountRadios.forEach(radio => {
     radio.addEventListener('click',  (evt) => {
         tipAmount = parseFloat(evt.target.value)
         calculate()
     })
 })
 
-document.getElementById('reset').addEventListener('click', () => {
-    uncheckRadios()
-
-    billInput.value = 0;
-    peopleInput.value = 0;
-    tipAmountCustom.value = 0;
-
-    bill        = 0;
-    people      = 0;
-    tipAmount   = 0;
-    totalPlaceholder.innerText = "0.00";
-    tipPlaceholder.innerText = "0.00";
+document.getElementById('reset-btn').addEventListener('click', () => {
+    document.getElementById("calculate-tip").reset()
 })
 
 function uncheckRadios() {
-    tipAmountInputs.forEach( radio =>
+    tipAmountRadios.forEach( radio =>
         radio.checked = false
     );
 }
 
+function setErrorMessage(field) {
+    if (document.getElementById(`${field.id}-msg-container`) !== null) {
+        document.getElementById(`${field.id}-msg-container`).innerText = field.title;
+    }
 
-function validate() {
+    if (field.id === "tip-amount-custom") {
+        document.getElementById(`tip-msg-container`).innerText = "Tip can't be negative";
+    }
 
 }
 
-function calculate() {
-    let total = bill + bill * tipAmount
-    let totalTip = bill * tipAmount
+function tipIsMissing() {
+    return tipAmountCustom.value.length === 0 && document.querySelectorAll('input[type=radio][name=tip-amount]:checked').length === 0;
+}
 
+function validate() {
+    cleanErrorMessages()
+    let fields = document.querySelectorAll(':invalid');
+
+    console.log(fields)
+
+    fields.forEach(field => setErrorMessage(field));
+
+    if(tipIsMissing()) {
+        document.getElementById(`tip-msg-container`).innerText = "Please select a tip or fill custom one";
+    }
+}
+
+function isValidated() {
+    return document.querySelectorAll(':invalid').length === 0 && !tipIsMissing()
+}
+
+function isNotANumber(value) {
+    return 'NaN' === value || 'Infinity' === value || undefined === value || 'undefined' === value || 'null' === value || null === value
+}
+
+
+function calculate() {
     validate()
 
-    tipPlaceholder.innerText = (totalTip / people).toFixed(2);
-    totalPlaceholder.innerText = (total / people).toFixed(2);
+    if (!isValidated()) {
+        return
+    }
+
+    let bill = document.getElementById('bill').value;
+    let people = document.getElementById('people').value;
+
+    let totalTip = parseFloat(bill) * parseFloat(tipAmount)
+    let total =  parseFloat(bill) + parseFloat(totalTip)
+
+    if (!isNotANumber(total.toString()) && !isNotANumber(totalTip.toString())) {
+        let tipPlaceholderResult = (totalTip / people).toFixed(2)
+        let totalPlaceholderResult = (total / people).toFixed(2)
+        tipPlaceholder.innerText = tipPlaceholderResult;
+        totalPlaceholder.innerText = totalPlaceholderResult;
+    }else {
+        document.getElementById('tip-amount-person-placeholder').innerText = "0.00"
+        document.getElementById('total-amount-person-placeholder').innerText = "0.00"
+    }
 }
