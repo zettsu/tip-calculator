ddb9595a4e6fc9d9c6c35d552d19a1c3aecfa877
Matias Olivera <jmatias.olivera@gmail.com>
2022-12-30 20:34:26 -0300

    fixed some css issues

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let tipAmount = 0;
+let tipAmount = 0
 
-let tipAmountRadios = document.querySelectorAll('input[type=radio][name=tip-amount]');
-let tipAmountCustom = document.getElementById('tip-amount-custom');
+let tipAmountRadios = document.querySelectorAll('input[type=radio][name=tip-amount]')
+let tipAmountCustom = document.getElementById('tip-amount-custom')
 let tipPlaceholder  = document.getElementById('tip-amount-person-placeholder')
 let totalPlaceholder = document.getElementById('total-amount-person-placeholder')
-
 
 tipAmountCustom.addEventListener('focus', () => {
     uncheckRadios()
@@ -14,9 +13,11 @@ function cleanErrorMessages() {
     document.querySelectorAll(`.error-msg`).forEach(msg => msg.innerText = "")
 }
 
-tipAmountCustom.addEventListener('input', (evt) => {
-    tipAmount = evt.target.value / 100;
-    calculate()
+['input','focusout'].forEach( event => {
+    tipAmountCustom.addEventListener(event, (evt) => {
+        tipAmount = evt.target.value / 100;
+        calculate()
+    });
 });
 
 document.getElementById('people').addEventListener('input', (evt) => {
@@ -50,25 +51,25 @@ function setErrorMessage(field) {
     }
 
     if (field.id === "tip-amount-custom") {
-        document.getElementById(`tip-msg-container`).innerText = "Tip can't be negative";
+        document.getElementById(`tip-msg-container`).innerText = "Tip can't be negative"
     }
 
 }
 
 function tipIsMissing() {
-    return tipAmountCustom.value.length === 0 && document.querySelectorAll('input[type=radio][name=tip-amount]:checked').length === 0;
+    return tipAmountCustom.value.length === 0 && document.querySelectorAll('input[type=radio][name=tip-amount]:checked').length === 0
 }
 
 function validate() {
     cleanErrorMessages()
-    let fields = document.querySelectorAll(':invalid');
+    let fields = document.querySelectorAll(':invalid')
 
     console.log(fields)
 
-    fields.forEach(field => setErrorMessage(field));
+    fields.forEach(field => setErrorMessage(field))
 
     if(tipIsMissing()) {
-        document.getElementById(`tip-msg-container`).innerText = "Please select a tip or fill custom one";
+        document.getElementById(`tip-msg-container`).innerText = "Please select a tip or fill custom one"
     }
 }
 
